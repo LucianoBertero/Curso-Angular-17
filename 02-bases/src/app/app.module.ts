469e1c97e816af2fadd3280fb5485ef57090b3e4
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-02-15 17:32:26 -0300

    seccion 4 terminada

---
+++ 02-bases/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter/counter.component';
+import { HeroComponent } from './heros/hero/hero.component';
+import { ListComponent } from './heros/list/list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CounterComponent,
+    HeroComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
