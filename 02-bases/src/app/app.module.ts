0e15fc5836aa4ccd447750b86c02ee68615741a4
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-02-16 00:03:59 -0300

    seccion 5 terminada

--- 02-bases/src/app/app.module.ts
+++ 02-bases/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter/counter.component';
 import { HeroComponent } from './heros/hero/hero.component';
 import { ListComponent } from './heros/list/list.component';
+import { DbzModule } from './dbz/dbz.module';
+import { MainPageComponent } from './dbz/pages/main-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
     HeroComponent,
-    ListComponent
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    DbzModule
 
   ],
   providers: [
