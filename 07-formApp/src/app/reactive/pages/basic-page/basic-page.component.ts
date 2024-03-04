64a34a8a4bcc3abdc1da1d7d799294c5947e46b7
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-03-04 20:41:42 -0300

    cambios

---
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: ``,
+})
+export class BasicPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: ['0', [Validators.required, Validators.min(0)]],
+    inStorage: ['0', [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  onSave() {
+    if (this.myForm.invalid) return;
+    console.log(this.myForm);
+  }
+}
