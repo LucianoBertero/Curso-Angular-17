fc7f381f29411962fb11d72b14b75ff7a6881359
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-03-05 00:04:17 -0300

    asd

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 1000,
+  inStorage: 10,
+};
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``,
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   public myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     price: ['0', [Validators.required, Validators.min(0)]],
@@ -13,9 +18,13 @@ export class BasicPageComponent {
   });
 
   constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
 
   onSave() {
     if (this.myForm.invalid) return;
     console.log(this.myForm);
+    this.myForm.reset({ price: 10, inStorage: 0 });
   }
 }
