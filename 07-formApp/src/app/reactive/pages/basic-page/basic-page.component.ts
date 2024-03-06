1e33e9a75c5093440946f0fe25378d40bb7d7ecd
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-03-06 18:51:43 -0300

    fin seccion 17

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -19,12 +19,40 @@ export class BasicPageComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {}
   ngOnInit(): void {
-    this.myForm.reset(rtx5090);
+    // this.myForm.reset(rtx5090);
   }
 
-  onSave() {
-    if (this.myForm.invalid) return;
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  onSave(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log(this.myForm);
     this.myForm.reset({ price: 10, inStorage: 0 });
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres`;
+
+          return 'null';
+      }
+    }
+    return 'hola mundo';
   }
 }
